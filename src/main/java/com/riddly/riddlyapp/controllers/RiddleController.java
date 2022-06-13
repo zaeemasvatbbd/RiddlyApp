54b837a74ab029321d71369892ede4df4e753fb1
Jordanvz <jordan@bbd.co.za>
2022-06-13 10:03:44 +0200

    changed User table to Player table

--- src/main/java/com/riddly/riddlyapp/controllers/RiddleController.java
+++ src/main/java/com/riddly/riddlyapp/controllers/RiddleController.java
@@ -4,8 +4,10 @@ import com.riddly.riddlyapp.models.Riddle;
 import com.riddly.riddlyapp.repositories.RiddleRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -25,5 +27,17 @@ public class RiddleController {
         return ResponseEntity.ok(riddles);
     }
 
+    @PostMapping("/riddle")
+    public ResponseEntity<String> addRiddle(@RequestBody Riddle riddle) {
+        if (riddle == null) return ResponseEntity.badRequest().build();
+        riddleRepository.save(riddle);
+        return ResponseEntity.ok(String.format("Riddle %s saved successfully", riddle));
+    }
 
+    @PostMapping("/riddle/answer")
+    public ResponseEntity<String> answerRiddle(@RequestBody Riddle riddle) {
+        if (riddle == null) return ResponseEntity.badRequest().build();
+        riddleRepository.save(riddle);
+        return ResponseEntity.ok(String.format("Answered riddle successfully"));
+    }
 }
