fd0b09c2717809a8997d2e79508ac37fb6df50f8
Jordanvz <jordan@bbd.co.za>
2022-06-13 18:03:13 +0200

    user to player change

--- RiddlyDatabase.sql
+++ RiddlyDatabase.sql
@@ -11,7 +11,7 @@ GO
 USE RiddlyDB;
 GO
 
-CREATE TABLE [Player] (
+CREATE TABLE Player (
 	username VARCHAR(20) PRIMARY KEY ,
 	email VARCHAR(100) NOT NULL,
 	points INT
@@ -21,8 +21,6 @@ CREATE TABLE Riddles (
 	riddleID INT PRIMARY KEY IDENTITY(1,1),
 	question VARCHAR(MAX) NOT NULL,
 	answer VARCHAR(50) NOT NULL,
-	username VARCHAR(20) NOT NULL,
-	FOREIGN KEY (username) REFERENCES [Player] (username)
 );
 
 CREATE TABLE Answered_Riddles (
@@ -39,13 +37,13 @@ VALUES  ('Amanda', 'amanda@gmail.com', 0),
 		('John', 'john@gmail.com', 20),
 		('Jack', 'jack@gmail.com',20)
 
-INSERT INTO [Riddles] (question, answer, username)
-Values('I’m tall when I’m young, and I’m short when I’m old. What am I?','Candle','Amanda'), 
-	  ('The more of this there is, the less you see. What is it?','Darkness','John'), 
-	  ('David’s parents have three sons: Snap, Crackle, and what’s the name of the third son?','David','Amanda'), 
-	  ('It stalks the countryside with ears that can’t hear. What is it?', 'Corn', 'John'), 
-	  ('I am an odd number. Take away a letter and I become even. What number am I?','Seven','John'), 
-	  ('If two’s company, and three’s a crowd, what are four and five?','Nine','Amanda')
+INSERT INTO [Riddles] (question, answer)
+Values('I’m tall when I’m young, and I’m short when I’m old. What am I?','Candle'),
+	  ('The more of this there is, the less you see. What is it?','Darkness'),
+	  ('David’s parents have three sons: Snap, Crackle, and what’s the name of the third son?','David'),
+	  ('It stalks the countryside with ears that can’t hear. What is it?', 'Corn'),
+	  ('I am an odd number. Take away a letter and I become even. What number am I?','Seven'),
+	  ('If two’s company, and three’s a crowd, what are four and five?','Nine')
 
 INSERT INTO [Answered_Riddles] (riddleID, username)
 VALUES	(1, 'Jack'),
